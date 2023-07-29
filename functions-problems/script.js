b2e68531745af84372a2a79c55405db16279f8a2
Larry-cool <larrycoolboy92@gmail.com>
2023-07-30 01:25:46 +0200

    Added comments to my code

---
+++ functions-problems/script.js
@@ -0,0 +1,25 @@
+
+
+const statement = "This is going to be a beautiful day";
+
+function findLongestWord(statement){
+//Step 1. Split the string into an array of strings
+  const splitWords = statement.split(" ");
+  
+// Step 2. Initiate a variable that will hold the length of the longest word.
+  let longestWord = 0;
+  
+// Step 3. Create the FOR loop.
+  for(let i = 0; i < splitWords.length; i++) {
+     console.log(splitWords[i].length)
+  
+  if(splitWords[i].length > longestWord){
+    longestWord = splitWords[i];
+      
+ 
+  
+     }
+   }
+  //Step 4. Return the longest word
+  return longestWord;
+}
