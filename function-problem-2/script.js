37b46c1531b798a9e3d4f4181a962110b2995b39
Larry-cool <larrycoolboy92@gmail.com>
2023-07-31 19:53:39 +0200

    I added 4 steps to solve problem 2

---
+++ function-problem-2/script.js
@@ -0,0 +1,20 @@
+//Write a JavaScript function that accepts a string as a 
+//parameter and counts the number of vowels within the string.
+
+//Step 1 : define a function that takes in a parameter (a string).
+
+function getVowels(str) {
+//step 2 : define a variable that's equal to an array of vowels.
+
+ const arrayOfVowels = ["a", "e", "i", "o", "u"];
+
+ // step 3 :Initiate a new variable to hold the number of vowels.
+
+ let numberOfvowels = 0;
+
+ // step 4 :lowercase the words so they're the same case as vowels array,
+ //then split into array of each letter
+
+
+
+}
