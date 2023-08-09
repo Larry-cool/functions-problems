c58278c8c85e2ba1d07e9d62a93c2dbb4d91919a
Larry-cool <larrycoolboy92@gmail.com>
2023-08-09 15:45:29 +0200

    changed the value of my variable from 0 to empty string

--- findLongestWord/script.js
+++ findLongestWord/script.js
@@ -5,26 +5,29 @@ Write a JavaScript function that
 accepts a string as a parameter and find the longest word within the string.
 */
 
-const statement = "This is going to be a beautiful day";
-
 function findLongestWord(statement){
 //Step 1. Split the string into an array of strings
   const splitWords = statement.split(" ");
   
 // Step 2. Initiate a variable that will hold the length of the longest word.
-  let longestWord = 0;
+  let longestWord = "";
   
 // Step 3. Create the FOR loop.
   for(let i = 0; i < splitWords.length; i++) {
-     console.log(splitWords[i].length)
+     //console.log(splitWords[i].length)
   
-  if(splitWords[i].length > longestWord){
+  if(splitWords[i].length > longestWord.length){
     longestWord = splitWords[i];
       
  
   
      }
+   
    }
+  console.log(longestWord)
   //Step 4. Return the longest word
   return longestWord;
 }
+
+
+findLongestWord("This is going to be a beautiful day")
