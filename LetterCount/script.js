ae60a0423341ba6bc981f670c29402018738ede2
Larry-cool <larrycoolboy92@gmail.com>
2023-08-10 16:24:20 +0200

    used str.CharAt in the for loop

---
+++ LetterCount/script.js
@@ -0,0 +1,24 @@
+/* Write a JavaScript function that accepts two arguments, a string and a letter and 
+ the function will count the number of occurrences of the specified letter within the 
+ string.
+ */
+
+
+// Step 1 : Write a Js function that accepts two arguments, a string and a letter.
+function stat(str, letter){
+  
+  // Step 2 : declare a variable to count the letter.
+  let letterCount = 0;
+  
+  //write a For loop.
+  for(x = 0; x < str.length; x++) {
+    if(str.charAt(x) == letter){
+      letterCount += 1;
+    }
+      
+  }
+  return letterCount;
+}
+
+
+ console.log(stat("moonschool.com", "o"));
