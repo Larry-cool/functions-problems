7425373cbd7c0a4906e2bc0384fd4007d959ebc4
Larry-cool <larrycoolboy92@gmail.com>
2023-09-14 10:22:38 +0200

    used (if and else if) to sum the array

---
+++ recursion-problem-2/script.js
@@ -0,0 +1,13 @@
+// Problem 2: Write a JavaScript program to compute the sum of an array of integers.
+
+function sumArray(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  else {
+    return arr.pop() + sumArray(arr);
+    
+  }
+};
+
+console.log(sumArray([1,2,3,4,5]))
